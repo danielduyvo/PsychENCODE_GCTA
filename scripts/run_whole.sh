bb01d99eb9b7a16773c825919e03f88d2b1732e5
Daniel Vo <danielduyvo@yahoo.com>
2021-08-14 07:03:25 -0700

    Update scripts/run_whole.sh to have proper naming scheme

--- scripts/run_whole.sh
+++ scripts/run_whole.sh
@@ -2,28 +2,34 @@
 
 # qsub options
 #$ -w e
-#$ -N GCTA_analysis_whole
+#$ -N EUR_SPC_gene_HRC_whole
 #$ -l h_data=8G,h_rt=1:00:00,highp
 #$ -pe shared 2
 #$ -cwd
 #$ -V
-#$ -o whole.log
-#$ -e whole.err
+#$ -o EUR_SPC_gene_HRC_whole.log
+#$ -e EUR_SPC_gene_HRC_whole.err
 #$ -m a
 #$ -M danieldu
-#$ -t 1-25774
+#$ -t 1-24905
+
+# #$ -t 1-24905
+# #$ -t 1-75000
+# #$ -t 75001-93293
+# 24905 genes
+# 93293 isoforms
 
 # Requires a complete GRM to be generated at data/${PROJECT}/output/grms/complete
 # Can be generated with the following line using the file data/${PROJECT}/output/grm_chrs/all_chrs.txt
 # gcta64 --make-grm-bin --thread-num $THREADS --mgrm data/${PROJECT}/output/grm_chrs/all_chrs.txt \
 #     --out data/${PROJECT}/output/grms/complete
 
-PROJECT="whole"
+PROJECT="EUR_SPC_gene_HRC_whole"
 THREADS=2
 LINE=$(sed -n ${SGE_TASK_ID}p data/${PROJECT}/input/phenotype_ids)
 echo $LINE | \
     (
-    read ID CHR START END W_START W_END
+    read ID CHR START END
     printf "ID: %s\n" $ID
 
         # Check if the chromosome is a sex chromosome
