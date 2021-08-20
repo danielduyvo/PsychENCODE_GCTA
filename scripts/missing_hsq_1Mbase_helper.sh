c8b42decb2dceb5282ed624be611ce8052f53602
Daniel Vo <danielduyvo@yahoo.com>
2021-08-19 17:56:46 -0700

    Add missing .hsq to script

--- scripts/missing_hsq_1Mbase_helper.sh
+++ scripts/missing_hsq_1Mbase_helper.sh
@@ -4,6 +4,6 @@
 > data/${PROJECT}/output/results/missing.txt
 for file in $(awk '{print $1}' data/${PROJECT}/input/phenotype_ids); do
     if [[ ! -e data/${PROJECT}/output/hsqs/${file}.hsq ]]; then
-        printf '%s\n' $file >> data/${PROJECT}/output/results/missing.txt
+        printf '%s.hsq\n' $file >> data/${PROJECT}/output/results/missing.txt
     fi
 done
