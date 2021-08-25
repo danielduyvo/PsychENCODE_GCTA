cee84422afdf9591fde111ccee87ba02c21e8a07
danielduyvo <danielduyvo@gmail.com>
2021-08-24 17:41:21 -0700

    Helper script for setting up bivariate run

---
+++ scripts/bivariate_helper.sh
@@ -0,0 +1,26 @@
+!#/usr/bin/env bash
+
+POP="EUR"
+PHENO="SPC_isoform"
+GENO="HRC"
+MODEL="bivariate_whole"
+PROJECT="${POP}_${PHENO}_${GENO}_${MODEL}"
+
+GENE="ENSG00000128891"
+
+awk -v GENE=$GENE '{if($0 ~ GENE) print NR " " $1}' \
+    data/${PROJECT}/input/phenotype_ids > \
+    data/${PROJECT}/input/${GENE}_ids
+
+mkdir -p data/${PROJECT}/output/${GENE}/fin_peds
+mkdir -p data/${PROJECT}/output/${GENE}/graphs
+mkdir -p data/${PROJECT}/output/${GENE}/grm_chrs
+mkdir -p data/${PROJECT}/output/${GENE}/grm_ranges
+mkdir -p data/${PROJECT}/output/${GENE}/grms
+mkdir -p data/${PROJECT}/output/${GENE}/hsqs
+mkdir -p data/${PROJECT}/output/${GENE}/mgrms
+mkdir -p data/${PROJECT}/output/${GENE}/results
+
+pushd data/$PROJECT/output/${GENE}/grms
+ln -s ../../../../../grms/${POP}_${GENO}/* ./
+popd
