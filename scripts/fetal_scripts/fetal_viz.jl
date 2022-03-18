3f105473f7f218799fb576d9bfe4d97b06e466b9
Daniel <danielduyvo@yahoo.com>
2022-03-18 03:43:29 -0700

    Visualize sQTL tri1

--- scripts/fetal_scripts/fetal_viz.jl
+++ scripts/fetal_scripts/fetal_viz.jl
@@ -7,6 +7,8 @@ tri1_path =
 "data/Trimester_1_EUR_25_HPC_gene_filtered_1Mbase_window/output/results/all_variance.txt"
 tri2_path = 
 "data/Trimester_2_EUR_15_HPC_gene_filtered_1Mbase_window/output/results/all_variance.txt"
+tri1_sqtl_path =
+"data/tri1/hg19_sQTL_15HPC/results/prelim_1Mbase_window.csv"
 adult_path =
 "data/EUR_SPC_gene_HRC_1Mbase_window/output/results/all_variance.txt"
 
@@ -17,9 +19,9 @@ readh2 = function(path::String)
                     types = [String, Float64, Float64, Float64, Float64, Float64, 
                              Float64, Float64, Float64, Float64, Float64, Float64, 
                              Float64, Float64, Float64, Float64, Int64 ])
-
 end
 tri1 = readh2(tri1_path)
+tri1sqtl = readh2(tri1_sqtl_path)
 tri2 = readh2(tri2_path)
 adult = readh2(adult_path)
 
@@ -140,6 +142,44 @@ Legend(f[1:2,2],
        ],
        ["Tri 1", "Tri 2"])
 save("tri1_vs_tri2_cis_h2.pdf", f)
+
+# Cis h2 for RNA-seq and sQTL for Trimester 1
+f = Figure(resolution = (1000, 1000))
+axtop = Axis(f[1,1])
+axbot = Axis(f[2,1])
+linkxaxes!(axtop, axbot)
+boxplotsplit2(axbot, tri1.Cis_h2, tri1sqtl.Cis_h2)
+overlapdensity2(axtop, tri1.Cis_h2, tri1sqtl.Cis_h2)
+Label(f[1, 1, Top()], "Tri 1 RNA-seq vs. sQTL Cis-h\u00b2", valign = :bottom,
+    padding = (0, 0, 5, 0))
+axbot.xlabel = "Cis-h\u00b2"
+axbot.yreversed = true
+Legend(f[1:2,2],
+       [
+        [LineElement(color = :teal, linestyle = nothing)],
+        [LineElement(color = :orange, linestyle = nothing)]
+       ],
+       ["RNA-seq", "sQTL"])
+save("tri1_rnaseq_vs_sqtl_cis_h2.pdf", f)
+
+# Trans h2 for RNA-seq and sQTL for Trimester 1
+f = Figure(resolution = (1000, 1000))
+axtop = Axis(f[1,1])
+axbot = Axis(f[2,1])
+linkxaxes!(axtop, axbot)
+boxplotsplit2(axbot, tri1.Trans_h2, tri1sqtl.Trans_h2)
+overlapdensity2(axtop, tri1.Trans_h2, tri1sqtl.Trans_h2)
+Label(f[1, 1, Top()], "Tri 1 RNA-seq vs. sQTL Trans-h\u00b2", valign = :bottom,
+    padding = (0, 0, 5, 0))
+axbot.xlabel = "Trans-h\u00b2"
+axbot.yreversed = true
+Legend(f[1:2,2],
+       [
+        [LineElement(color = :teal, linestyle = nothing)],
+        [LineElement(color = :orange, linestyle = nothing)]
+       ],
+       ["RNA-seq", "sQTL"])
+save("tri1_rnaseq_vs_sqtl_trans_h2.pdf", f)
 
 # Cis h2 and SE for adults and both trimesters
 f = Figure(resolution = (1000, 1000))
